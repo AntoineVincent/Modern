7ea602e4e1a775983656bdf9291d66a7f288728d
Antoine <anton51200@laposte.net>
2016-04-04 09:35:30 +0200

    first push

---
+++ Index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type='text/javascript' src='js/Modern.js'></script>
+		<link type='text/css' rel='stylesheet' href='./css/bootstrap.min.css'>
+		<link type='text/css' rel='stylesheet' href='./css/Modern.css'>
+		<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+		<title>Modern</title>
+		<meta charset='utf-8'>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	</head>
+	<body>
+		<header>
+			<?php
+				include 'header.php';
+			?>
+		</header>
+		<div id="slide1"></div>
+		<div id="slide2">
+			<div class="col-xs-1"></div>
+			<div class="col-xs-10">
+				<h1>Introduction</h1>
+				<p class="intro">Warhammer est un univers imaginaire d’heroic fantasy créé dans les années 1980 par la société Games Workshop et développé pour servir de cadre au jeu de figurines Warhammer Fantasy Battle (1983) ainsi que pour d'autres jeux, notamment le jeu de rôle Warhammer Fantasy Roleplay (1986) et son extension futuriste Warhammer 40,000 (1987).</p>
+				<img src="./img/duel.jpg" class="duel" alt="duel" />
+				<p class="intro">L'univers de Warhammer est inspiré entre autres par une littérature de fantasy telle qu'on la retrouve dans les récits de J. R. R. Tolkien et Michael Moorcock, mais aussi dans les jeux de rôle plus anciens à petite échelle comme Donjons et Dragons. Au mélange des épopées tolkienites et du chaos de Moorcock s'est ajouté une inspiration historique très forte. Le Vieux Monde de Warhammer fait très largement écho à l'Europe occidentale du XVIe siècle au niveau technologique et hiérarchique, avec une forte réminiscence de la pensée médiévale et son mode de vie sous certains aspects. La carte du vieux monde étant en fait une déformation des 6 continents de notre monde.</p>
+				<p class="intro">De nombreux peuples se partagent les territoires du monde de Warhammer. La plupart d'entre eux sont des humains originaires du Vieux Monde, d'Arabie, de Norsca, de Nippon, de Cathay etc. Il existe également de nombreuses autres races non humaines : certaines sont proches des humains biologiquement (comtes-vampires, ogres, halflings) ou physiquement (elfes et nains). D'autres races sont totalement à part, comme les races peaux-vertes (orques et gobelins), les skavens ou les hommes-lézards. En effet beaucoup de races se font la guerre, commandés par de grands chefs de guerre qui les mèneront à la victoire ou à la défaite. Une véritable bataille pour savoir qui dirigera la terre. En général, chaque armée possède un point fort. Certaines armées sont polyvalentes (l'armée de l'Empire en fait partie).</p>
+			</div>
+			<div class="col-xs-1"></div>
+		</div>
+		<div id="slide3">
+			<div class="row">	
+				<div class="col-xs-1"></div>
+				<div class="col-xs-10">
+					<h1 class="titlequi"> Qui suis-je ?</h1>
+					<img src="./img/me.jpg" class="myself" alt="Qui suis-je" />
+					<p class="qui"> Moi c'est Antoine, ou AnonySmith au choix. Jeune de 23ans je collectionne et peint des Warhammer depuis plus de 11ans. Je possède une collection de plus de 1000 figurines.</p>
+					<p class="qui"> Ce site est un site tuto/vitrine, je tacherais régulierement de créer des tutos et articles sur le modelisme, la peinture et le jeux de stratégie warhammer/warhammer 40K. J'ajouterais également régulierement des photos de mes plus belles oeuvres.</p>
+					<p class="qui"> Mon armée ce compose de beaucoup de figurines du seigneur des anneaux qui me serviront pour les articles modelisme. Pour warhammer je possède beaucoup de figurines issus des Haut-elfes, des Skavens et des démons du cahos.</p>
+				</div>
+				<div class="col-xs-1"></div>
+			</div>
+			<div class="row">
+				<div class="col-xs-2"></div>
+				<div class="col-xs-2 icone">
+					<img src="./img/ico1.jpg" class="icono" alt="icone"/>
+					<p class="comp">Peinture</p>
+				</div>
+				<div class="col-xs-1"></div>
+				<div class="col-xs-2 icone">
+					<img src="./img/ico2.jpg" class="icono" alt="icone"/>
+					<p class="comp">Modelisme</p>
+				</div>
+				<div class="col-xs-1"></div>
+				<div class="col-xs-2 icone">
+					<img src="./img/ico3.jpg" class="icono" alt="icone"/>
+					<p class="comp">Gameplay</p>
+				</div>
+				<div class="col-xs-2"></div>
+			</div>
+		</div>
+		<div id="slide4"></div>
+
+		<footer>
+			<div class="col-xs-4 foot">
+				<p>Copyright © 2016</p>
+				<p>All right reserved to</p>
+				<p><a href="https://www.games-workshop.com">Games-workshop</a></p>
+			</div>
+			<div class="col-xs-4 foot"></div>
+			<div class="col-xs-4 foot">
+				<p>Design and developpement :</p>
+				<p>built by AnonySmith</p>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="./img/logo.png" class="logo" alt="logo" /></a>
+    </div>
+    <div class="collapse navbar-collapse menu">
+      <ul class="nav navbar-nav">
+        <li class="re"><a class="link" href="#">acceuil</a></li>
+        <li class="re"><a class="link" href="#article">articles</a></li>
+        <li class="re"><a class="link" href="#photos">photos</a></li>
+        <li><a class="link" href="#contact">contact</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
